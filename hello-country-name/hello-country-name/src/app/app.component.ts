17dbcb265a74af69cc4bb753471b074522c62cd8
Featherless-Biped <114402536+Featherless-Biped@users.noreply.github.com>
2023-03-31 15:59:00 +0300

    removed useless dependencies

--- hello-country-name/hello-country-name/src/app/app.component.ts
+++ hello-country-name/hello-country-name/src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface Location {
+  country_name: string;
+  region_name: string;
+  city: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-country-name';
+  location$: Observable<Location | null> = null;
+  
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getLocation();
+  }
+
+  getLocation() {
+    const url = 'https://ipapi.co/json/';
+    this.location$ = this.http.get<Location>(url);
+  }
 }
